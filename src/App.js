067fd6ce4f263a1a50f195df427858858d13c2b4
vachaganaslanyan1992 <vachagan.aslanyan.1992@gmail.com>
2023-11-24 13:52:49 +0400

    feat: start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "./components/Menu";
+import Home from "./components/Home";
+import './index.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Menu></Menu>
+            <Home></Home>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/TrendingCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TrendingCarousel(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default TrendingCarousel;
